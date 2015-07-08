2b2d3742c015ea628c1d2e4261320b703b6cacb7
Jhonny Test <iriyaz93@gmail.com>
2015-07-08 06:05:48 +0530

    Without MVC

--- calC+/ViewController.swift
+++ calC+/ViewController.swift
@@ -10,16 +10,66 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var display: UILabel!
+    
+    var userIsIntheMiddleOfTypingANumber: Bool = false
+    
+    @IBAction func appendDigit(sender: UIButton) {
+        let digit = sender.currentTitle!
+        if userIsIntheMiddleOfTypingANumber {
+            display.text = display.text! + digit
+        } else {
+            display.text = digit
+            userIsIntheMiddleOfTypingANumber = true
+        }
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @IBAction func equals() {
+            userIsIntheMiddleOfTypingANumber = false
+            operandStack.append(displayValue)
     }
-
-
+    
+    func performOperation(operation : (Double, Double) -> Double) {
+        if operandStack.count >= 2 {
+            displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
+            equals()
+        }
+    }
+    
+    func performOperation1(operation : Double -> Double) {
+        if operandStack.count >= 1 {
+            displayValue = operation(operandStack.removeLast())
+            equals()
+        }
+    }
+    
+    var operandStack = Array<Double>()
+    
+    @IBAction func operate(sender: UIButton) {
+        let operation = sender.currentTitle!
+        if userIsIntheMiddleOfTypingANumber {
+            equals()
+        }
+        switch operation {
+            case "+" : performOperation { $0 + $1 }
+            case "−" : performOperation { $1 - $0 }
+            case "×" : performOperation { $0 * $1 }
+            case "÷" : performOperation { $1 / $0 }
+            case "√" : performOperation1 { sqrt($0)}
+            case "∏" : performOperation1 { $0 * M_PI }
+            case "%" : performOperation { $1 % $0 }
+            default : break
+        }
+    }
+    
+    var displayValue: Double {
+        get {
+            return NSNumberFormatter().numberFromString(display.text!)!.doubleValue
+        }
+        set {
+            display.text = "\(newValue)"
+        }
+    }
+    
 }
 
